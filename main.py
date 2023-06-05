53e1a8e0467a0b749ac584d0078594b9affc5da2
jerxy <jerxylol@gmail.com>
2023-06-05 20:22:57 +0200

    Return temperature for any station and date

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import pandas as pd
 
 app = Flask(__name__)  # a convention in flask
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[['STAID', 'STANAME                                 ']]
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
-@app.route("/api/v1/<station>/<date>")  # <> special syntax for url we use in flask
+@app.route("/api/v1/<weather_station>/<date>")  # <> special syntax for url we use in flask
 def station(weather_station, date):
-    temperature = 23
+    filename = "data/TG_STAID" + str(weather_station).zfill(6) + ".txt"  # stations nr is extended to 6 digits by zeros
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    # csv_date = f"{date[0:4]}-{date[4:6]}-{date[6:]}" if we wanted to add dashes for user
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": weather_station,
             "date": date,
             "temperature": temperature}
